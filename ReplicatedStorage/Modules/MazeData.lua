a579771fc975434aba26b56ffd10339ff6fee164
Muhammad Gidzane M. <116780769+Dzann@users.noreply.github.com>
2026-07-04 17:30:55 +0700

    Updated ReplicatedStorage/Modules/MazeData.lua

---
+++ ReplicatedStorage/Modules/MazeData.lua
@@ -0,0 +1,17 @@
+-- @ScriptType: ModuleScript
+local MazeData = {
+
+	Grid = nil,
+
+	Width = 0,
+	Height = 0,
+
+	Start = nil,
+	Finish = nil,
+
+	CellSize = 0,
+	Origin = Vector3.zero
+
+}
+
+return MazeData
